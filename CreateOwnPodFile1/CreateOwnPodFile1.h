ccd9b6242a8bb46c72cd85b0f7905246fa32c34b
Guru Mahan <iosdeve2022@gmail.com>
2023-08-09 17:15:43 +0530

    Initial Commit

---
+++ CreateOwnPodFile1/CreateOwnPodFile1.h
@@ -0,0 +1,18 @@
+//
+//  CreateOwnPodFile1.h
+//  CreateOwnPodFile1
+//
+//  Created by Guru Mahan on 09/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CreateOwnPodFile1.
+FOUNDATION_EXPORT double CreateOwnPodFile1VersionNumber;
+
+//! Project version string for CreateOwnPodFile1.
+FOUNDATION_EXPORT const unsigned char CreateOwnPodFile1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CreateOwnPodFile1/PublicHeader.h>
+
+
---
+++ CreateOwnPodFile1/Validator.swift
@@ -0,0 +1,12 @@
+//
+//  Validator.swift
+//  CreateOwnPodFile1
+//
+//  Created by Guru Mahan on 09/08/23.
+//
+
+import Foundation
+
+public class Validator {
+    public var name = "Guru Mahan"
+}
